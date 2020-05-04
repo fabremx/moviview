23df7892ab3ba70d361b2e05bd010bb7305f9c2e
Maxime Fabre <fabre.m96@gmail.com>
2020-05-05 00:59:21 +0200

    Updates

---
+++ precache-manifest.3ac9a7aac8fd021bddf30a7b9ed3c021.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e0039f4bd35ecc528d28d83f0203388a",
+    "url": "./index.html"
+  },
+  {
+    "revision": "ec3a4d1c011880bec72f",
+    "url": "./static/css/main.f8bcec44.chunk.css"
+  },
+  {
+    "revision": "561254f85e139729446d",
+    "url": "./static/js/2.2268ac01.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "./static/js/2.2268ac01.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ec3a4d1c011880bec72f",
+    "url": "./static/js/main.40e798b3.chunk.js"
+  },
+  {
+    "revision": "1c2039a03eed7a1f1c57",
+    "url": "./static/js/runtime-main.47a9b487.js"
+  },
+  {
+    "revision": "7c4c7b1c06861439e9b465d6ac49102e",
+    "url": "./static/media/list.7c4c7b1c.png"
+  },
+  {
+    "revision": "5a5875ed5dd4ecc52ca7fcdc92539281",
+    "url": "./static/media/loader.5a5875ed.gif"
+  },
+  {
+    "revision": "401a91b3e88bbf1f8f659ab01b47cd9c",
+    "url": "./static/media/not-available.401a91b3.png"
+  },
+  {
+    "revision": "147a304f71fa21e130d3f67a1d547eec",
+    "url": "./static/media/search-icon.147a304f.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/moviview/precache-manifest.0d5bec2e9a616c02a23520d3b0ad0e79.js"
+  "./precache-manifest.3ac9a7aac8fd021bddf30a7b9ed3c021.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/moviview/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
